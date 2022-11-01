4ac10fe785130ec5c866a22ef47f111e096e13ce
Gavin Osborn <osborng@xavier.edu>
2022-11-01 16:56:14 -0400

    removed mystery code at the bottom of the file

--- stress-load/src/components/Home.js
+++ stress-load/src/components/Home.js
@@ -49,22 +49,3 @@ const Home = () => {
 }
  
 export default Home;
-
-
-
-
-
-<header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
