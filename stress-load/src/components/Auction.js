573a619c6023e8af5139f961fb26061014447cdf
NotASuspiciousAccount <perronh@xavier.edu>
2022-12-06 21:37:55 -0500

    Revised comments
    The comments for each component should be more descriptive now.

--- stress-load/src/components/Auction.js
+++ stress-load/src/components/Auction.js
@@ -3,6 +3,14 @@ import '../App.css';
 import logo from '../logo.svg';
 import AuctionBrowser from './AuctionBrowser.js';
 
+/*
+    The Auction component is responsible for storing the data of a single auction.
+    Instances of this will be made by the AuctionCreator component,
+    and stored in the AuctionBrowser component.
+    This will need to store the type/quantity of food, the deadline the rescue
+    needs to be finished by, and the location of the food rescue, among other things.
+*/
+
 const Auction = () => {
 
     return (
