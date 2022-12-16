8d92ddd1ebba87b0ced5e7d2049da92971edb095
Gavin Osborn <osborng@xavier.edu>
2022-12-16 10:52:37 -0500

    initial commit for fastAPI template

---
+++ webAPI/main.py
@@ -0,0 +1,38 @@
+'''
+to run:
+uvicorn main:app --reload
+
+check:
+http://127.0.0.1:8000/items/5?q=somequery
+
+docs can be found at:
+http://127.0.0.1:8000/docs
+or
+http://127.0.0.1:8000/redoc
+'''
+from typing import Union
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+class Item(BaseModel):
+    name: str
+    quantity: int
+    is_offer: Union[bool, None] = None
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+    return {"item_name": item.name, "item_id": item_id}
